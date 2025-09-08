998b6adcae656c9a08e9477ed29e82e94e43b29c
jomrembau <jomir.bautista@gmail.com>
2025-09-08 15:30:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,24 @@ final_screen = GameOver()
 t_player = Player()
 vehicle = Cars()
 game_score = Score()
-difficulty = 100
+
+def game_on():
+    win.update()
+
+    vehicle.generate_cars()
+    vehicle.move_cars()
+
+    if t_player.michael.ycor() >= 250:
+        game_score.current_level += 1
+        t_player.reset_turtle()
+        game_score.update_score()
+
+    for car in vehicle.car_list:
+        if t_player.michael.distance(car["turtle"]) < 30:
+            final_screen.end_game()
+            return
+
+    win.ontimer(game_on, game_score.difficulty)
 
 win.setup(500,500)
 win.title("Turtle Crossing Game")
@@ -19,17 +36,7 @@ win.tracer(0)
 win.listen()
 win.onkey(t_player.move_up, "Up")
 
-def game_on():
-    win.update()
-    vehicle.move_cars()
-
-    if t_player.michael.ycor() >= 200:
-        game_score.current_level += 1
-        t_player.reset_turtle()
-        game_score.update_score()
-
-    win.ontimer(game_on, difficulty)
-
 game_on()
+
 
 win.exitonclick()
